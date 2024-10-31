a95f3ce0765a76a95a53fb08c76cc104f7d82ccc
Enrique Córcoles <X000201.empresas@ine.es>
2024-10-31 12:16:52 +0100

    primer commit

---
+++ src/main/java/com/example/demoCursoGit/DemoCursoGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoCursoGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCursoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCursoGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoCursoGit/DemoCursoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoCursoGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCursoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
